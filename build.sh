fe759aecd9ef9c361ad56d15e8e0198e658d6665
New <dierbekergasev6@gmail.com>
2026-06-05 13:47:01 +0500

    fix collectstatic2

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ set -o errexit
 
 pip install -r requirements.txt
 mkdir -p staticfiles
-python manage.py collectstatic --no-input
+echo "=== Checking static folder ==="
+ls -la static/ || echo "static/ NOT FOUND"
+ls -la static/css/ || echo "static/css/ NOT FOUND"
+python manage.py collectstatic --no-input --verbosity 2
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
