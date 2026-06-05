f08feeccc38777a96689676fa2623b02088c0487
New <dierbekergasev6@gmail.com>
2026-06-05 13:40:33 +0500

    fix collectstatic

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
+mkdir -p staticfiles
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
